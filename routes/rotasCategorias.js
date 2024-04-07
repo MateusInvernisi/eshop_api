0b79ff462b8ed814c029eaaa8eb1ee737708b8ac
MateusInvernisi <126488169+MateusInvernisi@users.noreply.github.com>
2024-04-07 16:30:37 -0300

    Commit inicial do projeto

---
+++ routes/rotasCategorias.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const { getCategorias, addCategoria, updateCategoria, 
+    deleteCategoria, getCategoriaPorCodigo } = 
+    require('../controllers/categoriaController');
+
+const rotasCategorias = new Router();
+
+rotasCategorias.route('/categoria')
+               .get(getCategorias)
+               .post(addCategoria)
+               .put(updateCategoria)
+               .delete(deleteCategoria);
+
+rotasCategorias.route('/categoria/:codigo') 
+               .get(getCategoriaPorCodigo)              
+               .delete(deleteCategoria);
+
+module.exports = { rotasCategorias };
